340d0468b08c85158fd24849cfaffed7a66843bc
Juan Rivera Vargas <jrivera.v@outlook.com>
2017-12-26 17:17:27 -0300

    Agregar archivos de proyecto.

---
+++ ApprendeApp/ApprendeApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ApprendeApp.Models;
+
+namespace ApprendeApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
